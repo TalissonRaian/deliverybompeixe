aa433003af21e7e3e9839e7f0b4d4af37d48bad6
TalissonRaian <105090196+TalissonRaian@users.noreply.github.com>
2022-07-23 00:33:39 -0400

    Inclusão de relogio dinamico
    relogio com condicional informando status da loja e horario de funcionamento

---
+++ script.js
@@ -0,0 +1,29 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hora = dateToday.getHours();
+    let minuto = dateToday.getMinutes();
+
+    if(hora < 10)  hora = '0' + hora;
+    if(minuto < 10)  minuto = '0' + minuto;
+
+    horas.textContent = hora;
+    minutos.textContent = minuto;
+
+    const onoff = document.querySelector('.status')
+    const fcio = document.querySelector('.funcionamento')
+
+    if (hora >= 10) {
+        onoff.textContent = "Estamos atendendo!"
+        fcio.textContent = "Aberto até as 15 horas"  
+    }
+    if(hora <= 15){
+        onoff.textContent = "Estamos Fechado"
+        fcio.textContent = "Aberto as 10 horas"
+    }
+    
+
+
+})
